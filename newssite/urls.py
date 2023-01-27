c0514d229cdabdccabf6bedb669ebf5ae0b59eb0
Enquil <jim.e.olesen@gmail.com>
2023-01-27 02:10:24 +0000

    rebuild without slug

--- newssite/urls.py
+++ newssite/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='index')
+    path('', views.PostList.as_view(), name='index'),
+    path('', views.PostDetail.as_view(), name='post_detail'),
 ]
