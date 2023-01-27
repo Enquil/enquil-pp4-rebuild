319a3fc3dbc6431a7781553d7942413b2a922bac
Enquil <jim.e.olesen@gmail.com>
2023-01-27 01:52:06 +0000

    rebuild

--- rtr/settings.py
+++ rtr/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary',
     'django_summernote',
+    'cloudinary',
     'newssite',
 ]
 
